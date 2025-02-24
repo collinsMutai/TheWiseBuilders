f83ad2b885a57574962a98660668d662883bacef
Collins Mutai <collinsfrontend@gmail.com>
2025-02-24 15:56:30 +0300

    ui

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, CommonModule],
+  imports: [FooterComponent, CommonModule, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
